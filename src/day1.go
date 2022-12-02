b8c1b45a84be4db28aa5700f9645819c57632536
Patrick Conway <patrick@titan.com>
2022-12-02 18:05:31 -0500

    part one done

--- src/day1.go
+++ src/day1.go
@@ -1,7 +1,101 @@
 package main
 
-import "fmt"
+/*
+--- Day 1: Calorie Counting ---
+The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies.
+One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's
+inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+This list represents the Calories of the food carried by five Elves:
+
+The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+The second Elf is carrying one food item with 4000 Calories.
+The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+The fifth Elf is carrying one food item with 10000 Calories.
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000
+(carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+*/
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// this reads the file into an array of stings
+// a single space would mean a new line, a double space would mean a new elf
+func readLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	check(err)
+
+	defer file.Close()
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+func findMaxElf(elfData []string) int {
+	var globalMax int = 0
+	var currVal int = 0
+
+	for _, element := range elfData {
+		if strings.TrimSpace(element) == "" {
+
+			if currVal > globalMax {
+				globalMax = currVal
+			}
+
+			currVal = 0
+		} else {
+			val, err := strconv.Atoi(element)
+			check(err)
+
+			currVal += val
+		}
+	}
+
+	return globalMax
+}
 
 func main() {
-	fmt.Println("hello world")
+	filename := "./data/day1"
+	data, err := readLines(filename)
+	check(err)
+
+	var elf int = findMaxElf(data)
+	fmt.Println("max elf is:", elf)
 }
