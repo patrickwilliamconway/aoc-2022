c97a79f88208458fe599dcfafddae55ffa2cb4fb
Patrick Conway <patrick@titan.com>
2022-12-03 12:38:27 -0500

    day 1 part 2

--- src/day1.go
+++ src/day1.go
@@ -37,10 +37,24 @@ In case the Elves get hungry and need extra snacks, they need to know which Elf 
 Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 */
 
+/*
+--- Part Two ---
+By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+
+To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one
+of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum
+of the Calories carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+*/
+
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -68,27 +82,35 @@ func readLines(filePath string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func findMaxElf(elfData []string) int {
-	var globalMax int = 0
+func elfSums(elfData []string) []int {
 	var currVal int = 0
+	var sums []int
 
 	for _, element := range elfData {
 		if strings.TrimSpace(element) == "" {
-
-			if currVal > globalMax {
-				globalMax = currVal
-			}
-
+			sums = append(sums, currVal)
 			currVal = 0
 		} else {
 			val, err := strconv.Atoi(element)
 			check(err)
-
 			currVal += val
 		}
 	}
 
-	return globalMax
+	return sums
+}
+
+func topThree(sums []int) int {
+	sort.Ints(sums)
+	var len = len(sums)
+	var topThree = sums[len-3 : len]
+
+	var sum int = 0
+	for _, element := range topThree {
+		sum += element
+	}
+
+	return sum
 }
 
 func main() {
@@ -96,6 +118,7 @@ func main() {
 	data, err := readLines(filename)
 	check(err)
 
-	var elf int = findMaxElf(data)
-	fmt.Println("max elf is:", elf)
+	var elfSums []int = elfSums(data)
+	var sum = topThree(elfSums)
+	fmt.Println("max elf is:", sum)
 }
