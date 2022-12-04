946ed8c680d172a998016972b9aeca2236e21034
Patrick Conway <patrick@titan.com>
2022-12-03 20:23:12 -0500

    day 2 part 2 - this code is horrible dont judge me

--- src/day2.go
+++ src/day2.go
@@ -67,6 +67,21 @@ func readLines(filePath string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+/*
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose,
+Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+*/
+
 func toRPS(played string) string {
 	switch played {
 	case "A", "X":
@@ -106,6 +121,36 @@ func determineWinner(p1 string, p2 string) int {
 	return score(p1Move, p2Move)
 }
 
+func determineMove(oppPlayed string, finish string) int {
+	if finish == "Y" { // draw
+		us := toRPS(oppPlayed)
+		them := toRPS(oppPlayed)
+		return score(us, them)
+	} else if finish == "X" { // loss
+		var us string
+		if oppPlayed == "A" { // rock
+			us = toRPS("Z") // scissors
+		} else if oppPlayed == "B" { // paper
+			us = toRPS("X") // rock
+		} else { // scissors
+			us = toRPS("Y") // paper
+		}
+		them := toRPS(oppPlayed)
+		return score(us, them)
+	} else { // win
+		var us string
+		if oppPlayed == "A" { // rock
+			us = toRPS("Y") // paper
+		} else if oppPlayed == "B" { // paper
+			us = toRPS("Z") // scissors
+		} else { // scissors
+			us = toRPS("X") // rock
+		}
+		them := toRPS(oppPlayed)
+		return score(us, them)
+	}
+}
+
 func main() {
 	filename := "./data/day2"
 	data, err := readLines(filename)
@@ -117,7 +162,8 @@ func main() {
 		moves := strings.Split(element, " ")
 		us := moves[1]
 		them := moves[0]
-		totalScore += determineWinner(us, them)
+		// totalScore += determineWinner(us, them)
+		totalScore += determineMove(them, us)
 	}
 
 	fmt.Println("total score:", totalScore)
